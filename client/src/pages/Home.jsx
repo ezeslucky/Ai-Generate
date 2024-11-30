fad1c90c752d14d8bbf586691a731a794362bae0
Lucky <lucky123@gmail.com>
2024-11-30 18:08:01 +0530

    description

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Description from "../components/Description"
 import Header from "../components/Header"
 import Steps from "../components/Steps"
 
@@ -7,6 +8,7 @@ function Home() {
     <div>
      <Header/>
      <Steps/>
+     <Description/>
     </div>
   )
 }
