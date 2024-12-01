06e8645e4b9820e657ebe4569ead72f1b964a3b3
Lucky <lucky123@gmail.com>
2024-12-01 18:06:55 +0530

    resuil stage 2

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,29 +1,47 @@
+import { useState } from "react"
 import { assets } from "../assets/assets"
 
 
-function Result() {
+const Result =()=> {
+
+  const [image,setImage] = useState(assets.sample_img_1)
+  const [isImageLoaded, setIsimageLoaded] = useState(false)
+
+  const [loading, setLoading] = useState(false)
+  const [input , setInput] = useState('')
+
+
+  const onsubmitHandler = async (e) => {
+
+  }
+
   return (
-    <form className=" flex flex-col min-h-[90vh] justify-center items-center " >
+    <form onSubmit={onsubmitHandler} className=" flex flex-col min-h-[90vh] justify-center items-center " >
     <div>
       <div className=" relative">
-        <img src={assets.sample_img_1} className=" max-w-sm rounded" alt="" />
-        <span className=" absolute bottom-0 left-0 h-1 bg-blue-500  w-full transition-all duration-[10s]"/>
+        <img src={image} className=" max-w-sm rounded" alt="" />
+        <span className= {` absolute bottom-0 left-0 h-1 bg-blue-500 & {loading ? ' w-full transition-all duration-[10s]' : ' w-0' } `}/>
 
       </div>
-      <p>Loading....</p>
+      <p className={!loading ? 'hidden' : ""}>Loading....</p>
     </div>
 
+{!isImageLoaded && 
     <div className=" flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full ">
-      <input type="text" placeholder="Describe what you want to generate" className="  flex-1 bg-transparent outline-none ml-8 max-sm:w-20  placeholder-color " />
+      <input  onChange={e => setInput(e.target.value)} value={input} type="text" placeholder="Describe what you want to generate" className="  flex-1 bg-transparent outline-none ml-8 max-sm:w-20  placeholder-color " />
 
       <button type="submit" className=" bg-zinc-900 px-10 sm:px-16 py-3 rounded-full ">Generate</button>
     </div>
+}
 
 
+{isImageLoaded && 
 <div className=" flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full">
-  <p className=" bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer ">Generate Another</p>
-  <a href="" download className=" bg-zinc-900 px-10 py-3 rounded-full cursor-pointer ">Download</a>
+  <p onClick={()=> {setIsimageLoaded(false)}} className=" bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer ">Generate Another</p>
+  <a href={image} download className=" bg-zinc-900 px-10 py-3 rounded-full cursor-pointer ">Download</a>
 </div>
+
+}
     </form>
   )
 }
