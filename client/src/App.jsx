294c5561b4099c29b5845c11c7f6409de4c32661
Lucky <lucky123@gmail.com>
2024-12-05 10:48:15 +0530

    animate in header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,21 @@ import BuyCredet from "./pages/BuyCredet"
 import Home from "./pages/Home"
 import Result from "./pages/Result"
 import Navbar from "./components/Navbar"
-// import Footer from "./components/Footer"
+import Footer from "./components/Footer"
+import Login from "./components/Login"
+import { useContext } from "react"
+import { AppContext } from "./context/AppContext"
+
 
 
 const  App = ()=> {
- 
+
+ const {showLogin} = useContext(AppContext)
 
   return (
     <div className=" px-5 sm:px-11 md:px-15 lg:px-30 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50" >
       <Navbar/>
+     { showLogin &&  <Login/>} 
     <Routes>
       <Route path="/"  element={<Home/>}/>
       <Route path="/result"  element={ <Result/>}/>
@@ -20,6 +26,7 @@ const  App = ()=> {
 
 
     </Routes>
+    <Footer/>
 
     </div>
   )
